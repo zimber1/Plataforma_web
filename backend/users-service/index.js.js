1190c53e20c560232cf595a254f8911089f64c6a
Ariel <ari@MacBook-Air-de-Ariel.local>
2026-02-01 21:18:57 -0600

    Setup del backend y configuracion de Node

--- backend/users-service/index.js.js
+++ backend/users-service/index.js.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const PORT = process.env.PORT || 3000;
-const name = 'api-gateway';
+const name = 'users-service';
 const server = http.createServer((req, res) => {
   if (req.url === '/health') {
     res.writeHead(200, { 'Content-Type': 'application/json' });
