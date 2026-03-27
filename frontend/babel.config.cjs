26ec5e979e815ba239bee410af3da88496437b23
Jesus Vega <jesus.vegmed@hotmail.com>
2026-03-27 00:50:30 -0600

    error

--- frontend/babel.config.cjs
+++ frontend/babel.config.cjs
@@ -2,5 +2,8 @@ module.exports = {
   presets: [
     ['@babel/preset-env', { targets: { node: 'current' } }],
     ['@babel/preset-react', { runtime: 'automatic' }]
+  ],
+  plugins: [
+    'babel-plugin-transform-import-meta'
   ]
 }
